1f8a0b17279a480a1b66979750eaa6b3d8763829
Walther Oliveira <waltherpires@hotmail.com>
2025-03-13 03:29:49 -0300

    Finished Cart Context and Reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
-import logoImg from '../assets/logo.jpg';
-import Button from './UI/Button';
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
 
-export default function Header(){
-    return <header id="main-header">
-        <div id="title">
-            <img  src={logoImg} alt="A restaurant" />
-            <h1>ReactFood</h1>
-        </div>
-        <nav>
-            <Button textOnly>Cart (0)</Button>
-        </nav>
+import CartContext from "../store/CartContext";
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  },
+  0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
     </header>
+  );
 }
