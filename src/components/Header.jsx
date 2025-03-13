5d6d3cc75c9b1965cc9a1820a6c41b0ae0255d0e
Walther Oliveira <waltherpires@hotmail.com>
2025-03-13 16:20:02 -0300

    open and close buttons cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,19 @@ import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
-  },
-  0);
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -19,7 +24,9 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -75,8 +75,6 @@ export function CartContextProvider({ children }) {
     removeItem
   };
 
-  console.log(cartContext);
-
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
