7e1c18fbece471962246b90238f07b8c3c671ee2
Walther Oliveira <waltherpires@hotmail.com>
2025-03-16 03:59:55 -0300

    Post request with Order Data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -25,7 +25,21 @@ export default function Checkout() {
   function handleSubmit(event) {
     event.preventDefault();
 
-    
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData
+        }
+      })
+    })
   }
 
   return (
@@ -33,7 +47,7 @@ export default function Checkout() {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
-        <Input label="Full name" type="text" id="full-name" />
+        <Input label="Full name" type="text" id="name" />
         <Input label="Email Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
