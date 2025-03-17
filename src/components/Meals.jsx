402c4111ce011fc08a85261b09b323428a3e6267
Walther Oliveira <waltherpires@hotmail.com>
2025-03-17 12:20:42 -0300

    Handling HTTP Loading and Error States

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,13 +1,18 @@
 import MealItem from "./MealItem";
 import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
 
 const requestConfig = {};
 
 export default function Meals() {
-  const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals', requestConfig, []);
+  const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/mealssss', requestConfig, []);
 
   if (isLoading) {
-    return <p>Fecthing meals...</p>
+    return <p className="center">Fecthing meals...</p>
+  }
+
+  if(error){
+    return <Error title="Failed to fetch meals" message={error} />
   }
 
   return (
